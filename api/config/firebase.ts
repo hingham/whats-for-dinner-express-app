bb60257d5e7789a943f003ed458381bf7d364051
Hannah Ingham <hannah.c.ingham@gmail.com>
2025-08-08 12:23:58 -0700

    adding vercel deploy docs

--- api/config/firebase.ts
+++ api/config/firebase.ts
@@ -4,8 +4,6 @@ import admin from 'firebase-admin';
 
 const projectId = process.env.PROJECT_ID || 'whats-for-dinner-f80b4';
 
-console.log({projectId, creds: process.env.GOOGLE_APPLICATION_CREDENTIALS})
-
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: `https://${projectId}.firebaseio.com`,
@@ -14,6 +12,5 @@ admin.initializeApp({
 
 // Document-based no sql datastorage (used instead of admin.database) for better query support
 const db = admin.firestore();
-console.log({db})
 
 export default db;
